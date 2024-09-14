4bc48b3a36e000796ed5a5be483e7098239cb655
AnnaMelekh <annasmelekhina@gmail.com>
2024-09-14 20:15:05 +0200

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Anna Melekhina on 14.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
